b2cb08a224c8879d97ad9af52cc1788435bd9b5b
jdryden-osi <jdryden@osisoft.com>
2017-03-27 14:45:48 -0400

    Moved bowtie template generation to main module
    Added New-PISysAudit_BowTieTemplate in core library. This function will
    read the JSON definition files and build the SVG template for the bow
    ties each time the module is run. This means that changes to the bowtie
    need only be made in the definition file and they will be reflected in
    the audit output.
    
    Also added Audit Item ID to bowtie, updated comments, and other
    miscellaneous fixes.

--- PISecurityAudit/Scripts/PISYSAUDIT/PISYSAUDITCORE.psm1
+++ PISecurityAudit/Scripts/PISYSAUDIT/PISYSAUDITCORE.psm1
@@ -5020,6 +5020,312 @@ END {}
 #***************************
 }
 
+function New-PISysAudit_BowTieTemplate
+{
+<#
+.SYNOPSIS
+Generates a Bow Tie diagram template. 
+.DESCRIPTION
+Accepts the path to a JSON file containing a BowTie definition,
+returns a Bow Tie diagram template in SVG format. 
+#>
+[CmdletBinding(DefaultParameterSetName="Default", SupportsShouldProcess=$false)]     
+param(
+		[parameter(Mandatory=$true, Position=0, ParameterSetName = "Default")]
+		[alias("def")]
+		[string]
+		$DefinitionPath,
+		[parameter(Mandatory=$false, ParameterSetName = "Default")]
+		[alias("dbgl")]
+		[int]
+		$DBGLevel = 0)
+BEGIN {}
+PROCESS
+{		
+	$fn = GetFunctionName
+
+	# Load JSON definition file and convert to JSON
+	$bowtieDefinition = Get-Content $DefinitionPath
+	try
+	{
+		$bowtieObject = $bowtieDefinition | ConvertFrom-Json
+	}
+	catch
+	{
+		$msg1 = "Failed to load BowTie definition file at $DefinitionPath"
+		Write-PISysAudit_LogMessage $msg "Error" $fn -eo $_
+		return $null
+	}
+
+	# Extract threats (left side) and impacts (right side)
+	$threats = $bowtieObject.Definition.Threats
+	$impacts = $bowtieObject.Definition.Impacts
+
+	# Defense/Reduction box height
+	$drHeight = 60
+
+	# Initialize SVG doc with headers and CSS styling
+	$svgDoc = @"
+	<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="1750" height="800" viewBox="0 0 1750 800" preserveAspectRatio="xMinYMin meet">
+	  <defs>
+		<style type="text/css"><![CDATA[
+		  text {
+			font-family: Segoe UI Light;
+			font-weight: bold;
+			font-size: 12px;
+			text-anchor: middle;
+		  }
+		  text.title {
+			font-size: 24px;
+		  }
+		  text.title2 {
+			font-size: 18px;
+		  }
+		  text.link {
+			text-decoration: underline;
+			fill: blue;
+		  }
+		  line {
+			stroke: #000000;
+			stroke-width: 3px;
+		  }
+		  rect {
+			stroke: #000000;
+			stroke-width: 1px;
+			fill: #FFFFFF;
+		  }
+		  rect.threat {
+			stroke-width: 5px;
+			stroke: #2B52FF;
+		  }
+		  rect.impact {
+			stroke-width: 5px;
+			stroke: #AA2020;
+		  }
+		  rect.pass {
+			fill: #45f442;  /* green */
+		  }
+		  rect.low {
+			fill: #fff428;  /* yellow */
+		  }
+		  rect.moderate {
+			fill: #ffb028;  /* orange */
+		  }
+		  rect.severe {
+			fill: #ff282f;  /* red */
+		  }
+		  rect.na {
+			fill: #93938f;  /* gray */
+		  }
+		]]></style>
+	  </defs>
+  
+	  <!-- Available substitutions: enclose in '%' to replace -->
+	  <!-- Title       : Replaced with file title -->
+	  <!-- Timestamp   : Replaced with report timestamp -->
+	  <!-- MachineName : Replaced with machine name -->
+	  <!-- AUxxxxx     : replaced with class name of Audit Item severity -->
+  
+	  <!-- Title Lines -->
+	  <title>%Title%</title>
+	  <text x="20" y="25" class="title" style="text-anchor:start;">%Title%
+		<tspan dx="10">—</tspan>
+		<tspan dx="10">%MachineName%</tspan>
+	  </text>
+	  <text x="20" y="50" class="title2" style="text-anchor:start;">%Timestamp%</text>
+
+"@
+
+	# Function to split text for line wrapping. 
+	# Accepts the unsplit text, the width (pixels), upper and lower
+	#     Y values (will be used to center text vertically), and a
+	#     scale. Scale is 1 for 12pt font, 1.5 for 18pt, etc.
+	# Returns array of lines, each with property 'text' and 'y'.
+	function SplitText($text, $width, $upperY, $lowerY, $scale=1)
+	{
+		# Average of 6.34px per character at 12pt font
+		if($text.Length -lt ($width / 6.34 / $scale))
+		{
+			$middle = $upperY + ($lowerY - $upperY)/2
+			$props = @{'text'=$text; 'y'=($middle+5)}
+			return New-Object PSCustomObject -Property $props
+		}
+		else
+		{
+			$splitText = $text -split ' '
+			$i=0
+			$workingStr = ''
+			$lineSplitStrs = @()
+			foreach($str in $splitText)
+			{
+				$workingStr += $str
+				if($workingStr.Length -gt ($width / 6.34 / $scale))
+				{
+					$lineSplitStrs += $workingStr.SubString(0, ($workingStr.Length - $str.Length)).TrimEnd()
+					$workingStr = $str + ' '
+				}
+				else { $workingStr += ' '}
+			}
+			$lineSplitStrs += $workingStr.TrimEnd()
+			$lineSplitStrs = $lineSplitStrs | Where-Object { $_ -NE '' }
+			$middle = $upperY + ($lowerY - $upperY)/2
+			$topLine = $middle - 20*($lineSplitStrs.Count/2) + 15
+			$y = $topLine
+			$ret = @()
+			foreach($splitStr in $lineSplitStrs)
+			{
+				$props = @{'text'=$splitStr; 'y'=$y}
+				$ret += New-Object PSCustomObject -Property $props
+				$y += 20
+			}
+			return $ret
+		}
+	}
+
+	# Initial X and Y values for padding
+	$initialX = 20
+	$initialY = 70
+
+	# Find Centerline, to hold middle circle
+	# Circle is centered vertically on the side with fewer branches. May not be less than 260.
+	if($threats.Count -ge $impacts.Count ) { $fewer = $impacts.Count } else { $fewer = $threats.Count }
+	$circleY = $initialY + 120*($fewer/2) - 10
+	if($circleY -lt 260) { $circleY = 260 }
+
+	# Longest threat branch sets width of left side
+	$maxThreats = 0
+	foreach($threat in $threats) {
+		if($threat.Defenses.Count -gt $maxThreats) { $maxThreats = $threat.Defenses.Count }
+	}
+
+	# Draw the left side of the bow tie
+	$y = $initialY + 50
+	$linesMeetX = $initialX + 180 + 120*$maxThreats
+	foreach($threat in $threats)
+	{
+		# Move x forward if not longest branch
+		$x = $initialX + 120*($maxThreats - $threat.Defenses.Count)
+	
+		# Lines first
+		$svgDoc += "`t<line x1=`"$x`" y1=`"$y`" x2=`"$($x+100+120*$threat.Defenses.Count)`" y2=`"$y`"/>`r`n"
+		$svgDoc += "`t<line x1=`"$($x+100+120*$threat.Defenses.Count)`" y1=`"$y`" x2=`"$linesMeetX`" y2=`"$circleY`"/>`r`n"
+		# Draw Branch name
+		$y -= 50
+		$svgDoc += "`t<rect x=`"$x`" y=`"$y`" width=`"100`" height=`"100`" class=`"threat`"/>`r`n"
+		$splitText = SplitText $threat.Name 100 $y ($y+100)
+		foreach($line in $splitText)
+		{
+			$svgDoc += "`t<text x=`"$($x+50)`" y=`"$($line.y)`">$($line.text)</text>`r`n"
+		}
+		# Draw defenses
+		$y += (50 - $drHeight/2)
+		foreach($defense in $threat.Defenses)
+		{
+			$x += 120
+			$svgDoc += "`t<rect x=`"$x`" y=`"$y`" width=`"100`" height=`"$drHeight`""
+			if($defense.AuditID) { $svgDoc += " class=`"%$($defense.AuditID)%`"" }
+			$svgDoc += "/>`r`n"
+			$splitText = SplitText $defense.Name 100 $y ($y+$drHeight)
+			foreach($line in $splitText)
+			{
+				$svgDoc += "`t<text x=`"$($x+50)`" y=`"$($line.y)`">$($line.text)</text>`r`n"
+			}
+			if($defense.AuditID)
+			{
+				$svgDoc += "`t<text x=`"$($x+50)`" y=`"$($y+$drHeight+12)`">$($defense.AuditID)</text>`r`n"
+			}
+		}
+
+		# Move down to next branch Y
+		$y += (120 + $drHeight/2)
+
+	} # End Left half
+
+	# Draw the Top Event and Hazard
+	$x = $linesMeetX + 75
+	$svgDoc += "`t<line x1=`"$x`" y1=`"$circleY`" x2=`"$x`" y2=`"$($circleY-160)`" style=`"stroke-width: 5px;`"/>`r`n"
+	$svgDoc += "`t<circle cx=`"$x`" cy=`"$circleY`" r=`"75`" fill=`"#F47D42`" stroke=`"#000000`" stroke-width=`"5px`"/>`r`n"
+	foreach($line in (SplitText $bowtieObject.TopEvent 150 ($circleY-75) ($circleY+75) 1.5))
+	{
+		$svgDoc += "`t<text x=`"$x`" y=`"$($line.y)`" class=`"title2`">$($line.text)</text>`r`n"
+	}
+	$svgDoc += "`t<rect x=`"$($x-85)`" y=`"$($circleY-185)`" width=`"170`" height=`"75`" style=`"stroke-width:5px;`"/>`r`n"
+	foreach($line in (SplitText $bowtieObject.Hazard 170 ($circleY-185) ($circleY-110) 1.5))
+	{
+		$svgDoc += "`t<text x=`"$x`" y=`"$($line.y)`" class=`"title2`">$($line.text)</text>`r`n"
+	}
+
+	# Draw the right half of the bow tie
+	$rightHalfX = $x + 155
+	$linesMeetX = $x + 75
+	$y = $initialY + 50
+	foreach($impact in $impacts)
+	{
+		$x = $rightHalfX
+
+		# Lines first
+		$svgDoc += "`t<line x1=`"$x`" y1=`"$y`" x2=`"$($x+100+120*$impact.Reductions.Count)`" y2=`"$y`"/>`r`n"
+		$svgDoc += "`t<line x1=`"$linesMeetX`" y1=`"$circleY`" x2=`"$rightHalfX`" y2=`"$y`"/>`r`n"
+
+		# Draw reductions
+		$y -= ($drHeight/2)
+		foreach($reduction in $impact.Reductions)
+		{
+			$svgDoc += "`t<rect x=`"$x`" y=`"$y`" width=`"100`" height=`"$drHeight`""
+			if($reduction.AuditID) { $svgDoc += " class=`"%$($reduction.AuditID)%`"" }
+			$svgDoc += "/>`r`n"
+			$splitText = SplitText $reduction.Name 100 $y ($y+$drHeight)
+			foreach($line in $splitText)
+			{
+				$svgDoc += "`t<text x=`"$($x+50)`" y=`"$($line.y)`">$($line.text)</text>`r`n"
+			}
+			if($reduction.AuditID)
+			{
+				$svgDoc += "`t<text x=`"$($x+50)`" y=`"$($y+$drHeight+12)`">$($reduction.AuditID)</text>`r`n"
+			}
+			$x += 120
+		}
+
+		# Draw impact name
+		$y -= (50 - $drHeight/2)
+		$svgDoc += "`t<rect x=`"$x`" y=`"$y`" width=`"100`" height=`"100`" class=`"impact`"/>`r`n"
+		$splitText = SplitText $impact.Name 100 $y ($y+100)
+		foreach($line in $splitText)
+		{
+			$svgDoc += "`t<text x=`"$($x+50)`" y=`"$($line.y)`">$($line.text)</text>`r`n"
+		}
+
+		# Move down to next branch Y
+		$y += 170
+
+	} # End right half
+
+	$maxImpacts = 0
+	foreach($impact in $impacts) {
+		if($impact.Reductions.Count -gt $maxImpacts) { $maxImpacts = $impact.Reductions.Count }
+	}
+	$maxX = $rightHalfX + 120 + 120*($maxImpacts)
+	$maxY = $y - 50
+
+	# Set ViewPort and ViewBox with maxX and maxY
+	$svgDoc = $svgDoc -replace '(svg version.*)width=\"\d+\"', ('$1width="{0}"' -f $maxX)
+	$svgDoc = $svgDoc -replace '(svg version.*)height=\"\d+\"', ('$1height="{0}"' -f $maxY)
+	$svgDoc = $svgDoc -replace '(svg version.*)viewBox=\".+?\"', ('$1viewBox="0 0 {0} {1}"' -f $maxX, $maxY)
+
+	# Close svg tag
+	$svgDoc += "</svg>"
+
+	return $svgDoc
+}	
+
+END {}
+
+#***************************
+#End of exported function
+#***************************
+}
+
 function Write-PISysAudit_BowTieDiagrams
 {
 <#
@@ -5074,12 +5380,12 @@ PROCESS
 		$generatedReports = @{}
 
 		# PI Data Archive: load template
-		$daTemplatePath = PathConcat -ParentPath $scriptsPath -ChildPath "Utilities\BowTie_PIDataArchive.svg"
-		$daTemplate = Get-Content -Path $daTemplatePath
+		$daDefinitionPath = PathConcat -ParentPath $scriptsPath -ChildPath "Utilities\BowTieDefinition_PIDataArchive.json"
+		$daTemplate = New-PISysAudit_BowTieTemplate -def $daDefinitionPath
 
 		# PI AF Server: load template
-		$afTemplatePath = PathConcat -ParentPath $scriptsPath -ChildPath "Utilities\BowTie_PIAFServer.svg"
-		$afTemplate = Get-Content -Path $afTemplatePath
+		$afDefinitionPath = PathConcat -ParentPath $scriptsPath -ChildPath "Utilities\BowTieDefinition_PIAFServer.json"
+		$afTemplate = New-PISysAudit_BowTieTemplate -def $afDefinitionPath
 
 		# PI Coresight: load template
 		# .......
